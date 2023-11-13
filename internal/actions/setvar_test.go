9fb1947fe2bf50e5b9a634c5e890a2c9ac53841f
Sebastien Blot <sebastien@crowdsec.net>
2023-11-13 11:04:56 +0100

    up

--- internal/actions/setvar_test.go
+++ internal/actions/setvar_test.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/corazawaf/coraza/v3/collection"
-	"github.com/corazawaf/coraza/v3/debuglog"
-	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
-	"github.com/corazawaf/coraza/v3/internal/corazawaf"
+	"github.com/crowdsecurity/coraza/v3/collection"
+	"github.com/crowdsecurity/coraza/v3/debuglog"
+	"github.com/crowdsecurity/coraza/v3/experimental/plugins/plugintypes"
+	"github.com/crowdsecurity/coraza/v3/internal/corazawaf"
 )
 
 type md struct {
