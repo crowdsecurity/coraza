cb4e44715e21cc462a632c99ff3e9c372d5976e4
Sebastien Blot <sebastien@crowdsec.net>
2025-03-31 09:51:10 +0200

    lint

--- internal/bodyprocessors/multipart.go
+++ internal/bodyprocessors/multipart.go
@@ -21,7 +21,7 @@ import (
 type multipartBodyProcessor struct{}
 
 func (mbp *multipartBodyProcessor) ProcessRequest(reader io.Reader, v plugintypes.TransactionVariables, options plugintypes.BodyProcessorOptions) error {
-	//Set REQUEST_BODY no matter what
+	// Set REQUEST_BODY no matter what
 	buf := new(strings.Builder)
 	if _, err := io.Copy(buf, reader); err != nil {
 		return err
